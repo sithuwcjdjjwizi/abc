fe1761b1fb2b0a27c91c51a8d7b620ee53319596
sithuwcjdjjwizi <sithumboy02@gmail.com>
2025-06-30 12:53:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+🌝🌝const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
